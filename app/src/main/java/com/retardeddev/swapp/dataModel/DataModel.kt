d7be59e4002cff527d4b22fe77e8831092c573d9
KaSaNaa <pasindudeshitha123@gmail.com>
2024-03-20 22:15:32 +0530

    working on API interface

---
+++ app/src/main/java/com/retardeddev/swapp/dataModel/DataModel.kt
@@ -0,0 +1,8 @@
+package com.retardeddev.swapp.dataModel
+
+data class SpaceWeatherEvent(
+    val title: String,
+    val startDate: String,
+    val endDate: String,
+    // Add other properties as needed
+)
