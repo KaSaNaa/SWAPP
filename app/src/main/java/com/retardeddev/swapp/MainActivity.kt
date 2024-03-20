0a1dc0b8c60087ba02c4a69ec256d4f42d1846ae
KaSaNaa <pasindudeshitha123@gmail.com>
2024-03-20 21:31:15 +0530

    template created for home UI

--- app/src/main/java/com/retardeddev/swapp/MainActivity.kt
+++ app/src/main/java/com/retardeddev/swapp/MainActivity.kt
@@ -4,9 +4,9 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,7 +19,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.retardeddev.swapp.R
 
 @Preview
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -76,8 +75,16 @@ fun AppTitle() {
 @Preview(showBackground = true)
 @Composable
 fun ContentBox() {
-    Text(color = Color.Black,
-        text = "Hello, World!"
-    )
-
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Content",
+            color = Color.Black,
+            textAlign = TextAlign.Center,
+        )
+    }
 }
